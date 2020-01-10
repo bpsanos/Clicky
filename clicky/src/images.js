eeeeedea4e0f77f8fa7e7220a7f98b5d6c556b65
Brianna Psanos <bpsanos@yahoo.com>
2020-01-09 20:52:10 -0600

    Still broken

---
+++ clicky/src/images.js
@@ -0,0 +1,26 @@
+import bats from './images/bats.png';
+import cat from './images/cat.png';
+import alfred from './images/alfred.png';
+import joker from './images/joker.png';
+import jimgordon from './images/jimgordon.png';
+import ivy from './images/ivy.png';
+import twoface from './images/twoface.png';
+import scarecrow from './images/scarecrow.png';
+import batgirl from './images/batgirl.png';
+import harley from './images/harley.png';
+
+
+const images = [
+   bats,
+   cat,
+   alfred,
+   joker,
+   jimgordon,
+   ivy,
+   twoface,
+   scarecrow,
+   batgirl,
+   harley
+];
+
+export default images;
