848dd618553a5031cf0d1b6ee3d0250909e7566e
Brianna Psanos <bpsanos@yahoo.com>
2020-01-09 20:58:44 -0600

    maybe fixed

--- clicky/src/index.js
+++ clicky/src/index.js
@@ -4,7 +4,7 @@ import BurgerGame from './components/BatmanGame'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BurgerGame />, document.getElementById('root'));
+ReactDOM.render(<BatmanGame />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
