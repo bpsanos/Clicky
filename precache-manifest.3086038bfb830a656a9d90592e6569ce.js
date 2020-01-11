833f8afb2101d25f10daf5f8c90bec2055b5f8ca
Brianna Psanos <bpsanos@yahoo.com>
2020-01-10 18:14:21 -0600

    Updates

---
+++ precache-manifest.3086038bfb830a656a9d90592e6569ce.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba79b664dbfdd6b0a72192760468e323",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "bdc5cf5e8057a6046421",
+    "url": "/clicky/static/css/main.988b5d15.chunk.css"
+  },
+  {
+    "revision": "e12680a52ae25ae2ea93",
+    "url": "/clicky/static/js/2.79167d9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky/static/js/2.79167d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdc5cf5e8057a6046421",
+    "url": "/clicky/static/js/main.7217d687.chunk.js"
+  },
+  {
+    "revision": "9d9dc7aaffb7bd547956",
+    "url": "/clicky/static/js/runtime-main.e2f69772.js"
+  },
+  {
+    "revision": "efef4d5e69f692abb22063d272eef671",
+    "url": "/clicky/static/media/alfred.efef4d5e.png"
+  },
+  {
+    "revision": "3c3b5262b926d0f3723664366caa0230",
+    "url": "/clicky/static/media/batgirl.3c3b5262.png"
+  },
+  {
+    "revision": "284ab5b80a31ad0fa5d350cf32fd7dac",
+    "url": "/clicky/static/media/bg.284ab5b8.png"
+  },
+  {
+    "revision": "7c75235d6254f5b0bd970031c22dc71a",
+    "url": "/clicky/static/media/cat.7c75235d.png"
+  },
+  {
+    "revision": "dce6c84b953e1a53951e073f7d81e365",
+    "url": "/clicky/static/media/harley.dce6c84b.png"
+  },
+  {
+    "revision": "fd2b0d936a39256b4873ab128e22b0fb",
+    "url": "/clicky/static/media/ivy.fd2b0d93.png"
+  },
+  {
+    "revision": "e4a65436cd5d35598db00a4c78f6c47f",
+    "url": "/clicky/static/media/jimgordon.e4a65436.png"
+  },
+  {
+    "revision": "e1c6fcf1319a4c1d94fde36bbfbc263c",
+    "url": "/clicky/static/media/joker.e1c6fcf1.png"
+  },
+  {
+    "revision": "63b4e256539e10ac73245edc01935a3a",
+    "url": "/clicky/static/media/scarecrow.63b4e256.png"
+  },
+  {
+    "revision": "a1a7d311e80044e1a6853d499c4ef5f6",
+    "url": "/clicky/static/media/twoface.a1a7d311.png"
+  }
+]);
